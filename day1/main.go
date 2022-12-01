df96a6eca94b0b27679729d1509d0c4b60fc9f25
Pavel Ohursov <cucumb3rpasha@gmail.com>
2022-12-01 15:53:16 +0200

    day 1: part 2

--- day1/main.go
+++ day1/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"day1/part1"
+	"day1/part2"
 	"fmt"
 	"log"
 	"os"
@@ -18,10 +19,24 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-	answer, err := part1.Solution(scanner)
+	part1Answer, err := part1.Solution(scanner)
 	if err != nil {
 		log.Fatal("failed to solve part 1: %w", err)
 	}
 
-	fmt.Printf("part 1: %d", answer)
+	file, err = os.Open("input.txt")
+	if err != nil {
+		log.Fatal("failed to read input: %w", err)
+	}
+
+	defer file.Close()
+
+	scanner = bufio.NewScanner(file)
+
+	part2Answer, err := part2.Solution(scanner)
+	if err != nil {
+		log.Fatal("failed to solve part 1: %w", err)
+	}
+
+	fmt.Printf("part 1: %d, part 2: %d", part1Answer, part2Answer)
 }
---
+++ day1/part2/solution.go
@@ -0,0 +1,52 @@
+package part2
+
+import (
+	"bufio"
+	"fmt"
+	"strconv"
+)
+
+func Solution(scanner *bufio.Scanner) (int, error) {
+	biggest, _, err := handleLine(scanner, [3]int{}, 0)
+	if err != nil {
+		return 0, fmt.Errorf("failed to handle line: %w", err)
+	}
+
+	sum := 0
+
+	for _, num := range biggest {
+		sum += num
+	}
+
+	return sum, nil
+}
+
+func handleLine(scanner *bufio.Scanner, biggest [3]int, elfCalories int) ([3]int, int, error) {
+	if !scanner.Scan() {
+		return biggest, elfCalories, nil
+	}
+
+	if scanner.Text() == "" {
+		if elfCalories > biggest[0] {
+			biggest[0] = elfCalories
+		}
+
+		if elfCalories > biggest[1] && elfCalories < biggest[0] {
+			biggest[2] = biggest[1]
+			biggest[1] = elfCalories
+		}
+
+		if elfCalories > biggest[2] && elfCalories < biggest[1] {
+			biggest[2] = elfCalories
+		}
+
+		return handleLine(scanner, biggest, 0)
+	}
+
+	num, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		return biggest, 0, fmt.Errorf("failed to parse number: %w", err)
+	}
+
+	return handleLine(scanner, biggest, elfCalories+num)
+}
