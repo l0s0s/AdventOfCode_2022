5e71e8e4bd757d320b9516f05428172520321704
Pavel Ohursov <cucumb3rpasha@gmail.com>
2022-12-04 19:45:07 +0200

    day 4: part 2

--- day4/main.go
+++ day4/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"day4/part1"
+	"day4/part2"
 	"fmt"
 	"log"
 	"os"
@@ -23,5 +24,19 @@ func main() {
 		log.Fatal("failed to solve part 1: %w", err)
 	}
 
-	fmt.Printf("part 1: %d", part1Answer)
+	file, err = os.Open("input.txt")
+	if err != nil {
+		log.Fatal("failed to read input: %w", err)
+	}
+
+	defer file.Close()
+
+	scanner = bufio.NewScanner(file)
+
+	part2Answer, err := part2.Solution(scanner)
+	if err != nil {
+		log.Fatal("failed to solve part 2: %w", err)
+	}
+
+	fmt.Printf("part 1: %d, part 2: %d", part1Answer, part2Answer)
 }
